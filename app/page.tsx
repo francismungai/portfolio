4d162273ef2384cb03af8d829101ac4ed8aeef03
francismungai <fwm25@case.edu>
2025-08-05 19:16:53 -0500

    updated resume

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
                     className="w-full sm:w-auto"
                   >
                     <a
-                      href="/Francis_Mungai_Resume_06032025.pdf"
+                      href="/Francis_Mungai_f29072025.pdf"
                       download
                       className="border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2"
                     >
